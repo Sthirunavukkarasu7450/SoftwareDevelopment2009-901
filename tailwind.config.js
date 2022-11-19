cf378898496e0b6c1fd090626f28f56f709fe831
Sthirunavukkarasu7450 <sharanthiru0103@gmail.com>
2022-11-18 21:00:36 -0500

    tailwind theme set

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,22 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        theme1: { 
+        "primary": "#00A5C8",
+        "secondary": "#7767c1",
+        "accent": "#8d7da6",
+        "neutral": "#fcf7f1",
+        "base-100": "#e0f2fe",
+        "info": "#339BEB",
+        "success": "#2EDCAA",
+        "warning": "#D17E0A",
+        "error": "#F73C2B",
+        },
+      },
+      
+    ],
+  },
 }
